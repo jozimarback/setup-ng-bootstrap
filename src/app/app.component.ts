6c58300ede004d96d841652edcaebc04b93230f0
jozimarback <jozimar.back@evosix.com.br>
2017-10-01 16:29:50 -0300

    Adicionado configuraçoes de setup e componentes bootstrap no app component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles:[`
+  .star {
+    font-size: 1.5rem;
+    color: #b0c4de;
+  }
+  .filled {
+    color: #1e90ff;
+  }
+`]
 })
 export class AppComponent {
   title = 'app works!';
